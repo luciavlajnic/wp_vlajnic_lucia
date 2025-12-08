a4811c544028ea2d2bf320760a02f10f0ba0cfa7
luciavlajnic <luciavlajnic@gmail.com>
2025-12-08 16:04:06 +0100

    js

---
+++ index.js
@@ -0,0 +1,32 @@
+/* alert("Ovo je moja prva JS stranica");
+prompt("Unesi svoje ime");
+let ime= prompt("Unesi svoje ime");
+alert("Tvoje ime je: " + ime);
+let broj =prompt("Unesi jedan broj");
+alert("Kvadrat toga broja je:" + (broj*broj));
+
+let broj1 =prompt("Unesi prvi broj");
+let broj2 =prompt("Unesi drugi broj");
+
+alert("KRazlika tih brojeva je:" + (broj1-broj2));
+
+let broj1 =prompt("Unesi prvi broj");
+let broj2 =prompt("Unesi drugi broj");
+if(broj1>broj2 )
+{
+        alert(broj1 + "je veći od " + broj2);
+}
+else if(broj1< broj2)
+{
+    alert(broj2 + "je veći od" + broj1)
+}
+else{
+    alert("brojevi su jednaki")
+}
+
+ */
+
+let masa = prompt("Unesi masu u kg;");
+let visina = prompt("Unesi visinu u m;");
+let bmi = masa/(visina*visina)
+alert("Vaš BMI iznosi: " + bmi);
